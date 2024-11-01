c39f0a9bd039e31b2c020c047c925076049e502a
kaylathornton55@icloud.com <kayla_thornton@Kaylas-MacBook-Air.local>
2024-11-01 11:56:22 -0400

    Implemented post request to create new avatar

--- create-your-team/src/pages/Create.jsx
+++ create-your-team/src/pages/Create.jsx
@@ -1,11 +1,45 @@
 import React from "react";
-import supabase from "../Client";
+import { useState } from "react";
 import { Link } from "react-router-dom";
+import supabase from "../Client";
 
 const Create = () => {
+    // stores all the user input
+    const [ userInput , setUserInput ] = useState(
+        {
+            name: '',
+            height: 0,
+            color: ''
+        } );
+
+    // stores user input for POST request
+    const handleUserInput = (e) => {
+        // takes all prev values of userInput and creates new obj w key=[e.target.name] and value
+        setUserInput({...userInput, [e.target.name]: e.target.value})
+    }
+    console.log(userInput)
+
+    const onCreate = async (e) => {
+        e.preventDefault()
+
+        // add new crewmate to database
+        const { data, error } = await supabase.from('CrewMates').insert([{name: userInput.name, height: userInput.height, color: userInput.color}]).select();
+        console.log(data)
+        // clear form
+        setUserInput({ name: '', height: 0, color: '' })
+    }
+
     return(
         <div>
-            Create Page
+            <form className="new-crew">
+                <label htmlFor="name">Name: </label>
+                <input placeholder="name" type="text" name="name" value={userInput.name} onChange={handleUserInput} />
+                <label htmlFor="height">Height (in ft): </label>
+                <input type="number" name="height" min='0' max='10' value={userInput.height} onChange={handleUserInput} />
+                <label htmlFor="color">Color: </label>
+                <input placeholder="color" type="text" name="color" value={userInput.color} onChange={handleUserInput} />
+                <button type="submit" onClick={onCreate}> Create! </button>
+            </form>
         </div>
     )
 }
